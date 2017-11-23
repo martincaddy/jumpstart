37e1ee69fdacb10632a2b4843f5c4406d3d9c822
Martin Caddy <martincaddy@gmail.com>
2017-11-23 20:16:49 +0000

    Completed project

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+  // HEADER FUNCTION
+
+     $(window).scroll(function () {
+         var $header = $('.navbar');
+         var $headerHeight = $(window).width() < 768 ? 135 : 50;
+         $(window).scrollTop() >= $headerHeight
+             ? $header.slideUp(150)
+             : $header.slideDown(150)
+     });
+
+// IMAGE CAROUSEL FUNCTION
+
   $('.carousel').slick({
     slidesToScroll: 1,
     autoplay: true,
