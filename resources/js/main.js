5ef423e5ed2909f7cf85b614c13f49772d09b05e
Martin Caddy <martincaddy@gmail.com>
2017-11-23 19:29:12 +0000

    23 Carousel all working

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,11 @@
- $('.carousel').slick({
-     arrows: false,
-     dragable: false,
-     pauseOnFocus: false,
-     pauseOnHover: false,
-     accessibility: false,
-     autoplay: true,
-     autoplaySpeed: 4000
- });
-});
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+  });
